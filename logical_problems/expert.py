f6bdeefee6b51c737a795a4035963c1ca5e94283
Ankita Mohanty <ankitamohanty1919191@gmail.com>
2025-07-22 18:48:56 +0530

    Added solution for Chef & NextGen and Expert Setter Problem

---
+++ logical_problems/expert.py
@@ -0,0 +1,23 @@
+'''Expert Setter
+A problem setter is called an expert if at least 50% of their problems are approved by Chef.
+
+Munchy submitted X problems for approval. If Y problems out of those were approved, find whether Munchy is an expert or not.
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of a two space-separated integers X and Y - the number of problems submitted and the number of problems that were approved by Chef.
+Output Format
+For each test case, output on a new line YES, if Munchy is an expert. Otherwise, print NO.
+
+The output is case-insensitive. Thus, the strings YES, yes, yeS, and Yes are all considered the same.'''
+
+
+t=int(input())
+while t>0:
+    x,y=map(int,input().split(" "))
+    res=(y/x)*100
+    if res>=50:
+        print("YES")
+    else:
+        print("NO")
+    t-=1    
